7ef0300329ce2b98bdaf5e39f856c509f934cd90
Arthur Wu <stationwu@163.com>
2018-02-13 14:15:48 +0800

    initial

---
+++ Stationkitties/src/main/java/com/cat/StationkittiesApplication.java
@@ -0,0 +1,12 @@
+package com.cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationkittiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationkittiesApplication.class, args);
+	}
+}
