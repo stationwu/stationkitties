c5fb34273ae116b60b7254015f8d2b44b47e9540
Arthur Wu <stationwu@163.com>
2018-02-22 17:51:36 +0800

    initial

--- src/main/java/com/cat/web/rest/KittyController.java
+++ src/main/java/com/cat/web/rest/KittyController.java
@@ -33,19 +33,19 @@ import com.cat.service.KittyService;
 @ExposesResourceFor(Kitty.class)
 public class KittyController {
 	public static final String PATH = "/Kitty";
-	
+
 	public static final String KITTY_PATH = PATH + "/{id}";
 
 	public static final String MARKET_PATH = "/Market";
-	
+
 	public static final String SALE_PATH = "/Sale";
-	
+
 	public static final String PURCHASE_PATH = "/Purchase";
-	
+
 	public static final String SALE_CANCEL_PATH = "/SaleCancel";
 
 	public static final String KITTY_ADD_PATH = PATH + "/Add";
-	
+
 	public static final String RECOVER_PATH = "/Recover";
 
 	@Autowired
@@ -57,63 +57,67 @@ public class KittyController {
 	@Autowired
 	private KittyService kittyService;
 
-//	@RequestMapping(path = KITTY_ADD_PATH, method = RequestMethod.GET)
-//	@Transactional
-//	public List<KittyContainer> addKittyToCustomer(HttpSession session) throws IOException, TranscoderException {
-//		String openCode = (String) session.getAttribute("OpenCode");
-//		Customer customer = customerRepository.findOneByOpenCode(openCode);
-//		Kitty kitty = kittyService.getRaddomKitty();
-//		kitty.setCustomer(customer);
-//		customer.addKitties(kittyRepository.save(kitty));
-//		CustomerContainer customerContainer = new CustomerContainer(customerRepository.save(customer));
-//		return customerContainer.getKitties();
-//	}
-	
+	// @RequestMapping(path = KITTY_ADD_PATH, method = RequestMethod.GET)
+	// @Transactional
+	// public List<KittyContainer> addKittyToCustomer(HttpSession session)
+	// throws IOException, TranscoderException {
+	// String openCode = (String) session.getAttribute("OpenCode");
+	// Customer customer = customerRepository.findOneByOpenCode(openCode);
+	// Kitty kitty = kittyService.getRaddomKitty();
+	// kitty.setCustomer(customer);
+	// customer.addKitties(kittyRepository.save(kitty));
+	// CustomerContainer customerContainer = new
+	// CustomerContainer(customerRepository.save(customer));
+	// return customerContainer.getKitties();
+	// }
+
 	@RequestMapping(path = MARKET_PATH, method = RequestMethod.GET)
 	@Transactional
-	public List<KittyContainer> getKittyMarket(Pageable pageRequest,HttpSession session) throws IOException, TranscoderException {
+	public List<KittyContainer> getKittyMarket(Pageable pageRequest, HttpSession session)
+			throws IOException, TranscoderException {
 		Page<Kitty> page = kittyRepository.findAllByIsForSaleIsTrueOrderByCustomerDescIdDesc(pageRequest);
-		//List<Kitty> kitties = kittyRepository.findAllByCustomerIsNull();
+		// List<Kitty> kitties = kittyRepository.findAllByCustomerIsNull();
 		ArrayList<KittyContainer> kittyContainers = page.getContent().stream().map(x -> new KittyContainer(x))
 				.sorted(Comparator.comparing(KittyContainer::getId)).collect(Collectors.toCollection(ArrayList::new));
 		return kittyContainers;
 	}
-	
+
 	@RequestMapping(path = KITTY_PATH, method = RequestMethod.GET)
 	@Transactional
 	public KittyContainer getKittyById(@PathVariable Long id) throws IOException, TranscoderException {
 		Kitty kitty = kittyRepository.findOne(id);
 		return new KittyContainer(kitty);
 	}
-	
+
 	@RequestMapping(path = SALE_PATH, method = RequestMethod.GET)
 	@Transactional
-	public KittyContainer saleKitty(@Param("openCode") String openCode,@Param("kittyId") Long kittyId,@Param("price") Double price) throws Exception {
+	public KittyContainer saleKitty(@Param("openCode") String openCode, @Param("kittyId") Long kittyId,
+			@Param("price") Double price) throws Exception {
 		Kitty kitty = kittyRepository.findOne(kittyId);
 		Customer customer = customerRepository.findOneByOpenCode(openCode);
-		if(customer == null)
+		if (customer == null)
 			throw new Exception("no customer");
 		kitty.setForSale(true);
 		kitty.setPrice(new BigDecimal(price));
 		return new KittyContainer(kittyRepository.save(kitty));
 	}
-	
+
 	@RequestMapping(path = PURCHASE_PATH, method = RequestMethod.GET)
 	@Transactional
-	public String purchaseKitty(@Param("openCode") String openCode,@Param("kittyId") Long kittyId) throws Exception {
+	public String purchaseKitty(@Param("openCode") String openCode, @Param("kittyId") Long kittyId) throws Exception {
 		Kitty kitty = kittyRepository.findOne(kittyId);
 		Customer customer = customerRepository.findOneByOpenCode(openCode);
-		if(customer == null){
+		if (customer == null) {
 			throw new Exception("no customer");
 		}
-		if(customer.getWallet().doubleValue() < kitty.getPrice().doubleValue()){
+		if (customer.getWallet().doubleValue() < kitty.getPrice().doubleValue()) {
 			throw new Exception("鱼干不足");
 		}
-		if(customer.getKitties().size()>9){
+		if (customer.getKitties().size() >= 9) {
 			throw new Exception("猫窝中的猫猫数量过多，最多只能养9只猫~");
 		}
 		Customer owner = kitty.getCustomer();
-		if(owner != null){
+		if (owner != null) {
 			owner.setWallet(new BigDecimal(owner.getWallet().doubleValue() + kitty.getPrice().doubleValue()));
 			owner.removeKitties(kitty);
 			customerRepository.save(owner);
@@ -126,27 +130,29 @@ public class KittyController {
 		customerRepository.save(customer);
 		return "购买成功";
 	}
-	
+
 	@RequestMapping(path = SALE_CANCEL_PATH, method = RequestMethod.GET)
 	@Transactional
-	public KittyContainer saleCancelKitty(@Param("openCode") String openCode,@Param("kittyId") Long kittyId) throws Exception {
+	public KittyContainer saleCancelKitty(@Param("openCode") String openCode, @Param("kittyId") Long kittyId)
+			throws Exception {
 		Kitty kitty = kittyRepository.findOne(kittyId);
 		Customer customer = customerRepository.findOneByOpenCode(openCode);
-		if(customer == null)
+		if (customer == null)
 			throw new Exception("no customer");
-		if(kitty.isForSale() == false){
+		if (kitty.isForSale() == false) {
 			throw new Exception("已经被买走咯~");
 		}
 		kitty.setForSale(false);
 		return new KittyContainer(kittyRepository.save(kitty));
 	}
-	
+
 	@RequestMapping(path = RECOVER_PATH, method = RequestMethod.GET)
 	@Transactional
-	public KittyContainer recoverlKitty(@Param("openCode") String openCode,@Param("kittyId") Long kittyId) throws Exception {
+	public KittyContainer recoverlKitty(@Param("openCode") String openCode, @Param("kittyId") Long kittyId)
+			throws Exception {
 		Kitty kitty = kittyRepository.findOne(kittyId);
 		Customer customer = customerRepository.findOneByOpenCode(openCode);
-		if(customer == null)
+		if (customer == null)
 			throw new Exception("no customer");
 		customer.removeKitties(kitty);
 		customerRepository.save(customer);
@@ -155,5 +161,5 @@ public class KittyController {
 		kitty.setPrice(new BigDecimal(500));
 		return new KittyContainer(kittyRepository.save(kitty));
 	}
-	
+
 }
