a9f9f86329c0ffe011797f1414853fa08225df4e
Arthur Wu <stationwu@163.com>
2018-03-07 18:02:37 +0800

    initial

--- src/main/java/com/cat/web/rest/KittyController.java
+++ src/main/java/com/cat/web/rest/KittyController.java
@@ -140,6 +140,8 @@ public class KittyController {
 		if (kitty.isForSale() == false) {
 			throw new Exception("已经被买走咯~");
 		}
+		customer.setWallet(customer.getWallet().add(new BigDecimal(100)));
+		customerRepository.save(customer);
 		kitty.setForSale(false);
 		return new KittyContainer(kittyRepository.save(kitty));
 	}
