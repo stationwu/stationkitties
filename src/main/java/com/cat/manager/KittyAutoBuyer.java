6f228e8ecdc5b0a4d020c0dd36eef37e222d2157
stationwu <stationwu@163.com>
2018-03-20 00:15:00 +0800

    initial

--- src/main/java/com/cat/manager/KittyAutoBuyer.java
+++ src/main/java/com/cat/manager/KittyAutoBuyer.java
@@ -34,7 +34,7 @@ public class KittyAutoBuyer {
 			public void run() {
 				while(true) {
 					try{
-						Thread.sleep(10000);
+						Thread.sleep(1800000);
 					} catch (InterruptedException e) {
 						
 					}
